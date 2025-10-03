425b3e4a2695b8c1d591e630bcc7ca10689ecc50
Bori888 <ilyes.bori8@gmail.com>
2025-10-03 12:32:17 +0200

    main jav

---
+++ main.js
@@ -0,0 +1,7 @@
+import ToDoModell from "./modell/ToDoModell.js";
+import ToDoView from "./view/ToDoView.js";
+import ToDoController from "./controller/ToDoController.js";
+
+const modell = new ToDoModell();
+const view = new ToDoView();
+const controller = new ToDoController(modell, view);
